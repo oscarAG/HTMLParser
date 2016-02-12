6ee42c7a4e1a5a2b6277c84b5092f559a7f8eedc
Oscar <garcia.oa94@gmail.com>
2016-02-11 22:05:13 -0600

    Continued efforts on parsing. Parsing irrelevant information from the html. Keep only 'https' and 'http' links in the html. Strip away empty tags.

--- src/com/example/parser/Parser.java
+++ src/com/example/parser/Parser.java
@@ -2,7 +2,6 @@ package com.example.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,22 +16,20 @@ public class Parser extends HtmlUtilities
     {
         ArrayList<String> urls = initUrls(); // init list of urls
 
+        // loop through all URL's
         for (String url : urls)
-        {   // loop through all URL's
+        {
             System.out.println("URL " + urls.indexOf(url) + ": " + url); // current url index
-            Document doc = Jsoup.connect(url).get();        // call to the page and get html
-            removeAllAttributes(doc.getAllElements());      // remove all attributes of each element in the html doc
-            System.out.println(doc.title());                //print the title
+            Document doc = Jsoup.connect(url).get(); // call to the page and get html
+            removeComments(doc); //remove all comments from the page
+            removeIrrelevantTags(doc); //remove predetermined irrelevant tags (ex. head)
+            removeIrrelevantAttributes(doc); // remove all irrelevant attributes of each element in the html doc
+            removeEmptyTagPairs(doc); //remove empty tag pairs after the doc has been stripped
+            System.out.println("Title: " + doc.title());    //print the title
+            System.out.println(doc);
+
             ArrayList<Elements> title_tags = initTags(doc); //list of all tags to consider titles
-            for(Elements title_type: title_tags)
-            {       // loop through tag types in the array list (ex. h1, then h2, then h3)
-                for(Element tag: title_type)
-                {   // loop through all of a tag type (ex. all of the h1)
-                    Elements tag_siblings = tag.siblingElements();  // init the tag's children
-                    tag_siblings.forEach(System.out::println);      // loop through the tag, print all children
-                }
-            }
-            System.out.println();
+
         }
     }
     
