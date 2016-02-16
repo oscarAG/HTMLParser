74d3afe75e8eea039b8072cef9541cce9f17fea3
Oscar <garcia.oa94@gmail.com>
2016-02-15 20:58:43 -0600

    Code cleanup!
    This code is useless if others can't read and understand it!

--- src/com/example/parser/HtmlUtilities.java
+++ src/com/example/parser/HtmlUtilities.java
@@ -13,32 +13,7 @@ import java.util.ArrayList;
  */
 public class HtmlUtilities
 {
-    //Init the list of urls parse from
-    //TODO:Get all URLS from a text file instead, and init array dynamically
-    public static ArrayList<String> initUrls()
-    {
-        ArrayList<String> urls = new ArrayList<>(); //array list of URL's
-        //urls.add("http://example.com/");
-        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/cost-of-attendance/index.htm");
-        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/financial-aid/index.htm");
-        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/tuition-fees/index.htm");
-        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/scholarships/index.htm");
-        return urls;
-    }
-
-    //Init the list header tags
-    public static ArrayList<Elements> initTags(Document doc)
-    {
-        // Get all elements of each tag
-        ArrayList<Elements> hierarchy = new ArrayList<>();
-        hierarchy.add(doc.select("h1"));
-        hierarchy.add(doc.select("div"));
-        hierarchy.add(doc.select("h2"));
-        hierarchy.add(doc.select("h3"));
-        return hierarchy;
-    }
-
-    //Remove attributes from every element in the document
+    //Remove attributes from every element in the document, unless it meets specific requirements
     //TODO: Keep URLS for crawling
     public static void removeIrrelevantAttributes(Document doc)
     {
@@ -52,53 +27,74 @@ public class HtmlUtilities
                 }
                 else
                 {   //href tag found, do operations for urls
-                    if(a.getValue().contains("https") || a.getValue().contains("http"))
-                    {   //if "https" or "http" was found in the url of the href
-                        if(!verifyUrl(a.getValue()))
-                        {   //if the url is bad
-                            e.removeAttr(a.getKey()); //remove
-                        }
+                    if(isBlacklistedKeyword(e))
+                    {   //Check if the it's a blacklisted tag, and if it is, if it satisfies the requirements for being deleted
+                        e.remove();
                     }
                     else
-                    {   //the link is unusable, delete
-                        e.removeAttr(a.getKey());
+                    {   //it's not blacklisted, so make sure it's a proper url
+                        if (a.getValue().contains("https") || a.getValue().contains("http")) {   //if "https" or "http" was found in the url of the href
+                            if (!verifyUrl(a.getValue())) {   //if the url is bad
+                                e.removeAttr(a.getKey()); //remove
+                            } //else it's all good, keep it in
+                        } else {   //the link is unusable, delete
+                            e.removeAttr(a.getKey());
+                        }
                     }
                 }
             }
-            //Check if the it's a blacklisted tag, and if it is, if it satisfies the requirements for being deleted
-            if(isBlacklistedKeyword(e))
-            {
-                e.remove();
-            }
         }
     }
 
-    //Remove irrelevant tags
-    public static void removeIrrelevantTags(Document doc)
+    //Check if a url is valid, mark for deletion if not
+    //Helper to removeIrrelevantAttributes(...)
+    private static boolean verifyUrl(String url)
     {
-        //initialize an array of the tags not desired in the document
-        ArrayList<Elements> tags_to_remove = new ArrayList<>();
-        tags_to_remove.add(doc.select("head"));
-        tags_to_remove.add(doc.select("figure"));
-        tags_to_remove.add(doc.select("script"));
-        tags_to_remove.add(doc.select("form"));
-        tags_to_remove.add(doc.select("img"));
-        tags_to_remove.add(doc.select("article"));
-        tags_to_remove.add(doc.select("header"));
-        tags_to_remove.add(doc.select("nav"));
-        tags_to_remove.add(doc.select("footer"));
-        tags_to_remove.add(doc.select("br"));
-        for(Elements el: tags_to_remove)
+        //check for social media
+        return !(url.contains("facebook") ||
+                url.contains("twitter") ||
+                url.contains("linkedin") ||
+                url.contains("youtube"));
+    }
+
+    //Check if an element is blacklisted to be deleted
+    //TODO: Preferably should have an array of blacklisted keywords, and loop through to delete.
+    //Helper to removeIrrelevantAttributes(...)
+    private static boolean isBlacklistedKeyword(Element e)
+    {
+        String content = e.text().toLowerCase(); //get the text inside for checking
+        //check if this is an 'a' tag, and if it has no attributes
+        if(e.tagName().equals("a"))
+        {
+            if(e.attributes().size() == 0 ||
+                    content.contains("give") ||
+                    content.contains("alumni") ||
+                    content.contains("athletics") ||
+                    content.contains("apply") ||
+                    content.contains("bookstore"))
+            {return true;}
+        }
+        return false;
+    }
+    /****************************************************************************************************/
+    //Remove irrelevant tags
+    public static void removeSpecificTags(ArrayList<Elements> list)
+    {
+        for(Elements el: list)
         {   //for each tag
             el.forEach(Element::remove); //remove all
         }
-        //unwrap unnecessary tags which you wish to keep the text from
-        doc.select("i").unwrap();
-        doc.select("strong").unwrap();
-        doc.select("em").unwrap();
-        doc.select("abbr").unwrap();
     }
-
+    /****************************************************************************************************/
+    //Unwrap the tags that you wish to keep the text from
+    public static void unwrapSpecificTags(ArrayList<Elements> list)
+    {
+        for(Elements el: list)
+        {   //for each tag
+            el.forEach(Element::unwrap);
+        }
+    }
+    /****************************************************************************************************/
     //Remove comments
     public static void removeComments(Node node)
     {
@@ -113,15 +109,7 @@ public class HtmlUtilities
             }
         }
     }
-
-    //Check if a url is valid, mark for deletion if not
-    public static boolean verifyUrl(String url)
-    {
-        //check for social media
-        return !(url.contains("facebook") || url.contains("twitter") || url.contains("linkedin") ||
-                 url.contains("youtube"));
-    }
-
+    /****************************************************************************************************/
     public static void removeEmptyTagPairs(Document doc)
     {
         //remove empty tag pairs
@@ -131,23 +119,7 @@ public class HtmlUtilities
                 .filter(e -> !e.hasText() && e.isBlock())
                 .forEach(Element::remove); //remove empty pairs
     }
-
-    //Check if an element is blacklisted to be deleted
-    //TODO: Preferably should have an array of blacklisted keywords, and loop through to delete.
-    public static boolean isBlacklistedKeyword(Element e)
-    {
-        String content = e.text().toLowerCase(); //get the text inside for checking
-        //check if this is an 'a' tag, and if it has no attributes
-        if(e.tagName().equals("a"))
-        {
-            if(e.attributes().size() == 0 || content.contains("give") ||
-                    content.contains("alumni") || content.contains("athletics") ||
-                    content.contains("bookstore"))
-            {return true;}
-        }
-        return false;
-    }
-
+    /****************************************************************************************************/
     //unwrap nested tags with only one child
     //TODO: Check for lists with only one element, and replace it with a 'p' tag.
     public static void unwrapNestedRedundancies(Document doc)
--- src/com/example/parser/Parser.java
+++ src/com/example/parser/Parser.java
@@ -2,8 +2,12 @@ package com.example.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /**
  * HTML Parser
@@ -11,24 +15,75 @@ import java.util.ArrayList;
  */
 public class Parser extends HtmlUtilities
 {
+    private static ArrayList<Elements> tags_to_remove, tags_to_unwrap;
+    private static ArrayList<String> urls;
+
     public static void main(String[] args) throws IOException
     {
-        ArrayList<String> urls = initUrls(); // init list of urls
-
+        setUrls();
         // loop through all URL's
         for (String url : urls)
         {
-            System.out.println("URL " + urls.indexOf(url) + ": " + url); // current url index
-            Document doc = Jsoup.connect(url).get(); // call to the page and get html
-            System.out.println("Title: " + doc.title());    //print the title
+            Document doc = Jsoup.connect(url).get(); //Retrieve HTML from the url
+            System.out.println("URL " + urls.indexOf(url) + ": " + url); //Print the index and URL
+            System.out.println("Title: " + doc.title()); //print the title of the page
+            setTagsToRemove(doc);
+            setTagsToUnwrap(doc);
             removeComments(doc); //remove all comments from the page
-            removeIrrelevantTags(doc); //remove predetermined irrelevant tags (ex. head)
-            removeIrrelevantAttributes(doc); // remove all irrelevant attributes of each element in the html doc
-            removeEmptyTagPairs(doc); //remove empty tag pairs after the doc has been stripped
+            removeSpecificTags(Parser.tags_to_remove); //remove predetermined tags (ex. head, script)
+            unwrapSpecificTags(Parser.tags_to_unwrap); //unwrap predetermined tags that you need the text from
+            removeIrrelevantAttributes(doc); // remove irrelevant attributes of each element in the html
+            removeEmptyTagPairs(doc); //remove empty tag pairs
             unwrapNestedRedundancies(doc); //unwrap nested tags with only one child
-            System.out.println(doc); //print the doc after all operations
-            System.out.println(); //new line
+            System.out.println(doc + "\n"); //print the doc for review
         }
     }
-    
+
+    //Initialize a list of URLs to be called to
+    //TODO:Get all URLS from a text file instead, and init array dynamically
+    public static void setUrls()
+    {
+        Parser.urls = new ArrayList<>(); //array list of URL's
+        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/financial-aid/index.htm");
+        urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/scholarships/index.htm");
+        //urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/cost-of-attendance/index.htm");
+        //urls.add("http://www.utrgv.edu/en-us/admissions/paying-for-college/tuition-fees/index.htm");
+    }
+
+    //Initialize a list of all tags to be completely removed from the document, along with their child elements
+    public static void setTagsToRemove(Document doc)
+    {
+        Parser.tags_to_remove = new ArrayList<>();
+        ArrayList<String> tags = new ArrayList<>();
+        tags.add("head");
+        tags.add("figure");
+        tags.add("script");
+        tags.add("form");
+        tags.add("img");
+        tags.add("article");
+        tags.add("header");
+        tags.add("nav");
+        tags.add("footer");
+        tags.add("br");
+        tags_to_remove //Get all tags to remove into the elements array list
+                .addAll(tags.stream()
+                .map(doc::select)
+                .collect(Collectors.toList()));
+    }
+
+    //Initialize a list of all tags to be unwrapped, child elements will be kept intact
+    public static void setTagsToUnwrap(Document doc)
+    {
+        Parser.tags_to_unwrap = new ArrayList<>();
+        ArrayList<String> tags = new ArrayList<>();
+        tags.add("i");
+        tags.add("strong");
+        tags.add("em");
+        tags.add("abbr");
+        tags_to_unwrap //Get all tags to remove into the elements array list
+                .addAll(tags.stream()
+                .map(doc::select)
+                .collect(Collectors.toList()));
+    }
+
 }
