e387b1d6afce01fbe1f2cfda4c88e03e88669dff
Oscar <garcia.oa94@gmail.com>
2016-03-07 22:23:39 -0600

    Queue being created.

---
+++ src/com/example/parser/UrlClass.java
@@ -0,0 +1,32 @@
+package com.example.parser;
+
+
+/**
+ * This is a custom class that will be used to keep track of when a URL was last visited.
+ * Created by oscar on 3/7/16.
+ */
+public class UrlClass {
+    private long time;
+    private String url, domain;
+    private boolean visited;
+
+    public UrlClass(long time, String url, String domain)
+    {
+        this.time = time;
+        this.url = url;
+        this.domain = domain;
+        this.visited = false;
+    }
+
+    public long getTime(){return time;}
+    public String getUrl(){return url;}
+    public String getDomain(){return domain;}
+    public void setVisited(boolean blah)
+    {
+        this.visited = blah;
+    }
+    public boolean isVisited()
+    {
+        return this.visited;
+    }
+}
